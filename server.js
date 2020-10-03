e5dc5ba4f65c364d04c4ccb412cdf6decfba4889
sungodadi <63474000+sungodadi@users.noreply.github.com>
2020-10-03 16:47:28 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = parseInt(req.query.second_number)+parseInt(req.query.first_number)
+   console.log(response);
+
+   res.end("Sum = " + JSON.stringify(response));
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
